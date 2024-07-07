0b4e8c03adc1770dbbc5c522d4a064044894a09f
Excel Chukwu <excel.chukwu1.0@gmail.com>
2024-07-07 03:43:22 +0200

    integrate search by title with cli
    Signed-off-by: Excel Chukwu <excel.chukwu1.0@gmail.com>

--- arxiv_retriever/cli.py
+++ arxiv_retriever/cli.py
@@ -1,6 +1,6 @@
 import typer
 from typing_extensions import Annotated
-from fetcher import fetch_papers
+from fetcher import fetch_papers, search_paper_by_title
 
 app = typer.Typer(no_args_is_help=True)
 
@@ -18,7 +18,28 @@ def fetch(categories: Annotated[list[str], typer.Argument(help="ArXiv categories
             typer.echo(f"    Authors: {', '.join(paper['authors'])}")
             typer.echo(f"    Published: {paper['published']}")
             typer.echo(f"    Link: {paper['link']}")
-            typer.echo(f"    Summary: {paper['summary'][:100]}...")  # truncate summary # TODO: possibly update to find index of first period character in summary then use for truncation. makes summary more complete.
+            typer.echo(
+                f"    Summary: {paper['summary'][:100]}...")  # truncate summary # TODO: possibly update to find index of first period character in summary then use for truncation. makes summary more complete.
+    except Exception as e:
+        typer.echo(f"An error occurred: {str(e)}", err=True)
+
+
+@app.command()
+def search(
+        title: Annotated[str, typer.Argument(help="ArXiv title to search for")],
+        limit: int = typer.Option(10, help="Maximum number of papers to search")
+):
+    """Search papers by title."""
+    typer.echo(f"Searching for papers matching {title}...")
+
+    try:
+        papers = search_paper_by_title(title, limit)
+        for i, paper in enumerate(papers, 1):  # TODO: extract this into helper function to eliminate duplicate
+            typer.echo(f"\n{i}. {paper['title']}")
+            typer.echo(f"    Authors: {', '.join(paper['authors'])}")
+            typer.echo(f"    Published: {paper['published']}")
+            typer.echo(f"    Link: {paper['link']}")
+            typer.echo(f"    Summary: {paper['summary'][:100]}...")
     except Exception as e:
         typer.echo(f"An error occurred: {str(e)}", err=True)
 
