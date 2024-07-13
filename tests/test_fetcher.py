8519ef1eb534cac5a3d04407ab6d1794468cc35d
Excel Chukwu <excel.chukwu1.0@gmail.com>
2024-07-13 14:31:32 +0200

    add test for search_paper_by_title function
    Signed-off-by: Excel Chukwu <excel.chukwu1.0@gmail.com>

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from typing import List, Dict
 
 from arxiv_retriever.fetcher import fetch_papers, search_paper_by_title, parse_arxiv_response
@@ -73,5 +74,29 @@ def test_fetch_papers_success(mock_requests, mock_parse_arxiv_response, mocker: 
     assert result == expected_parsed_result
 
 
-def test_search_paper_by_title_success():
-    pass
+def test_search_paper_by_title_success(mock_requests, mock_parse_arxiv_response, mocker: MockerFixture):
+    # Arrange
+    title = "Attention Is All You Need"
+    limit = 5
+    encoded_title = urllib.parse.quote_plus(title)
+    query = (f"http://export.arxiv.org/api/query?search_query=ti:{encoded_title}&sortBy=relevance&sortOrder=descending"
+             f"&max_results={limit}")
+
+    mock_response = mocker.Mock()
+    mock_response.status_code = 200
+    mock_response.text = '<fake_xml>Fake ArXiv response</fake_xml>'
+    mock_requests.return_value = mock_response
+
+    expected_parsed_result = [{'title': 'Attention Is All You Need', 'authors': ['John Doe']}]
+    mock_parse_arxiv_response.return_value = expected_parsed_result
+
+    # Act
+    result = search_paper_by_title(title, limit)
+
+    # Assert
+    assert encoded_title == "Attention+Is+All+You+Need"
+    assert query == (f"http://export.arxiv.org/api/query?search_query=ti:Attention+Is+All+You+Need&sortBy=relevance"
+                     f"&sortOrder=descending&max_results=5")
+    mock_requests.assert_called_once_with(query)
+    mock_parse_arxiv_response.assert_called_once_with(mock_response.text)
+    assert result == expected_parsed_result
