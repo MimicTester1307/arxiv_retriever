c23674f61aed2522c757fb3bf05de7c887d931af
Excel Chukwu <excel.chukwu1.0@gmail.com>
2024-07-15 22:55:14 +0200

    create rag package; add llm interface for interacting with Claude API
    Signed-off-by: Excel Chukwu <excel.chukwu1.0@gmail.com>

---
+++ rag/llm_interface.py
@@ -0,0 +1,24 @@
+import os
+from anthropic import Anthropic
+
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
+
+
+def get_llm_response(prompt: str) -> str:
+    """Prompt Claude 3.4 Sonnet for a response"""
+    client = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1024,
+        temperature=0,
+        system="You are a helpful AI assistant specializing in summarizing scientific papers, extracting the most "
+               "meaningful parts of the paper. You can also suggest future research directions that is grounded in "
+               "factual and currently available research.",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return message.content[0].text
